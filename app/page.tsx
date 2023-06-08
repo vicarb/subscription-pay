7e80ede681d7233435247e516985a7b56593f3d9
vicarb <vcardenasb@protonmail.com>
2023-06-08 14:03:31 -0400

    updated services

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Services from '@/components/Services/Services'
 import Landing from '@/components/Landing/Landing'
+import PricingTable from '@/components/Plan/Plan'
 export default function Home() {
   return (
    <>
    <Landing/>
-    <Services/>
+
+    <PricingTable/>
     </>
   )
 }
---
+++ components/Plan/Plan.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+
+const Plan = ({ plan, price, features }) => (
+  <div className="flex flex-col bg-white shadow-md rounded-lg p-6 m-3 flex-grow max-w-sm">
+    <h2 className="text-2xl font-bold mb-4 text-center">{plan}</h2>
+    <h3 className="text-xl font-semibold text-gray-500 mb-6 text-center">${price} / month</h3>
+    <ul className="mb-6 space-y-2 flex-grow">
+      {features.map((feature, index) => (
+        <li key={index} className="flex items-center">
+          <FontAwesomeIcon icon={faCheckCircle} className="text-green-500 h-4 w-4" />
+          <span className="ml-2">{feature}</span>
+        </li>
+      ))}
+    </ul>
+    <button className="mt-auto py-2 px-4 bg-blue-600 hover:bg-blue-500 text-white rounded">
+      Subscribe Now
+    </button>
+  </div>
+);
+
+const PricingTable = () => {
+  const plans = [
+    {
+      name: 'Starter Pack',
+      price: 100,
+      features: ['Up to 5 pieces of content', 'One revision per piece', 'Turnaround time within a week']
+      // add more features as needed
+    },
+    {
+      name: 'Growth Plan',
+      price: 200,
+      features: ['Up to 20 pieces of content', 'Two revisions per piece', 'Turnaround time within 3-5 days', 'Quarterly content strategy consultation']
+      // add more features as needed
+    },
+    {
+      name: 'Enterprise Solution',
+      price: 400,
+      features: ['Unlimited pieces of content', 'Unlimited revisions', 'Turnaround time within 1-3 days', 'Monthly content strategy consultation', 'Access to premium features']
+      // add more features as needed
+    }
+  ];
+
+  return (
+    <div className="flex flex-col items-stretch md:flex-row md:justify-center md:flex-wrap">
+      {plans.map((plan, index) => (
+        <Plan key={index} plan={plan.name} price={plan.price} features={plan.features} />
+      ))}
+    </div>
+  );
+};
+
+export default PricingTable;
